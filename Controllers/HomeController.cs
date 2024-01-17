ce087064e2bb743c8e8a20c533d4294a3f2be096
Khanaliyeva <gunelxaneliyeva@gmail.com>
2024-01-17 19:25:07 +0400

    last

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,23 @@
-﻿using Maxim.Models;
+﻿using Maxim.DAL;
+using Maxim.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Maxim.Controllers
 {
     public class HomeController : Controller
-    {        
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var services = _context.Services.ToList();
+
+            return View(services);
         }
     }
 }
